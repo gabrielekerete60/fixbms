6a5e9f0d7adfbe502917467c8d8495db4da80f07
Gabriel Ekerete <gabrielekerete60@gmail.com>
2025-07-17 17:12:20 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/next.config.ts
@@ -0,0 +1,24 @@
+import type {NextConfig} from 'next';
+
+// Force rebuild to resolve chunking issue
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
