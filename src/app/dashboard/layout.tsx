b741b812e6e2a92fbe7be8df52707aba7e69480d
Gabriel Ekerete <gabrielekerete60@gmail.com>
2025-07-31 11:29:51 +0000

    I can't scroll the main page, but let the sidebar bot scroll down while

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -372,8 +372,8 @@ export default function DashboardLayout({
   }
 
   return (
-    <div className="grid h-screen w-full overflow-hidden md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <div className="hidden border-r bg-muted/40 md:flex md:flex-col">
+    <div className="grid h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <div className="hidden border-r bg-muted/40 md:flex md:flex-col h-screen">
         <div className="flex h-14 shrink-0 items-center border-b px-4 lg:h-[60px] lg:px-6">
           <Link href="/dashboard" className="flex items-center gap-2 font-semibold">
             <Pizza className="h-6 w-6 text-primary" />
@@ -405,7 +405,7 @@ export default function DashboardLayout({
           </Card>
         </div>
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col h-screen">
         <header className="flex h-14 shrink-0 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
           <Sheet>
               <SheetTrigger asChild>
