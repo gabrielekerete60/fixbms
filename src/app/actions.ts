9a66eef7f765409fa7fd7bc26591a8e908ea9bee
Gabriel Ekerete <gabrielekerete60@gmail.com>
2025-08-06 17:04:23 +0000

    still showing this? the verification error

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -4,7 +4,6 @@
 import { doc, getDoc, collection, query, where, getDocs, limit, orderBy, addDoc, updateDoc, Timestamp, serverTimestamp, writeBatch, increment, deleteDoc, runTransaction, setDoc } from "firebase/firestore";
 import { startOfMonth, endOfMonth, startOfWeek, endOfWeek, startOfDay, endOfDay, endOfYear, eachDayOfInterval, format, subDays, endOfHour, startOfHour } from "date-fns";
 import { db } from "@/lib/firebase";
-import fetch from 'node-fetch';
 import { randomUUID } from 'crypto';
 import speakeasy from 'speakeasy';
 
