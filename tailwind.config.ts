085572cf81dba2da84913986c9eed3a58caeab04
Gabriel Ekerete <gabrielekerete60@gmail.com>
2025-08-08 00:00:35 +0000

    make it more slower, 1 s

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -91,8 +91,8 @@ export default {
         },
       },
       animation: {
-        'accordion-down': 'accordion-down 0.4s ease-out',
-        'accordion-up': 'accordion-up 0.4s ease-out',
+        'accordion-down': 'accordion-down 1s ease-out',
+        'accordion-up': 'accordion-up 1s ease-out',
       },
     },
   },
