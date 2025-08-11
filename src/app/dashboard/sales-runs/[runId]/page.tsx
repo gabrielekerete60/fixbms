bd27a1cc7229e7c04ab48fb3a9feef65a5f25a87
Gabriel Ekerete <gabrielekerete60@gmail.com>
2025-08-11 12:48:02 +0000

    still empty

--- src/app/dashboard/sales-runs/[runId]/page.tsx
+++ src/app/dashboard/sales-runs/[runId]/page.tsx
@@ -20,7 +20,7 @@ import { collection, getDocs, doc, addDoc, Timestamp, onSnapshot } from 'firebas
 import { db } from '@/lib/firebase';
 import { Badge } from '@/components/ui/badge';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogHeader, AlertDialogTitle, AlertDialogDescription, AlertDialogFooter, AlertDialogTrigger } from '@/components/ui/alert-dialog';
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogHeader, AlertDialogTitle, AlertDialogDescription, AlertDialogTrigger } from '@/components/ui/alert-dialog';
 import type PaystackPop from '@paystack/inline-js';
 import { Separator } from '@/components/ui/separator';
 import { format } from "date-fns";
@@ -890,4 +890,3 @@ function SalesRunDetails() {
 }
 
 export default SalesRunDetails;
-
